c304963a8c54f72b67587246018a3b0e70022c49
luuna <luna@hackclub.app>
2025-01-07 13:35:50 +0800

    not working idk why

--- src/randomorg_ellipticobj/DOCUMENTATION.md
+++ src/randomorg_ellipticobj/DOCUMENTATION.md
@@ -1,6 +1,6 @@
 # docs (not well made pluh!)
 
-## basic implementation
+# basic implementation
 import the module 
 ```python
 import randomorg
@@ -17,5 +17,30 @@ generate a number
 randomgen.randint()
 ```
 
-## additional parameters
-tba
+# commands
+
+## randint
+
+randint generates a random integer  
+
+### arguments
+
+**minimum**
+minimum number that can be generated (inclusive)  
+
+**maximum**
+maximum number that can be generated (inclusive)  
+should be at least one more than minimum  
+
+**numofints**
+number of integers that are generated.  
+should be positive  
+default value is 1
+
+**allowduplicates**
+only applies when multiple numbers are generated  
+when set to true, the numbers that are generated can contain duplicate numbers
+default value is true
+
+
+# exceptions
--- src/randomorg_ellipticobj/main.py
+++ src/randomorg_ellipticobj/main.py
@@ -1,40 +1,64 @@
-import os
 import requests
-import json
-from typing import List
-
+from typing import Union, List
 
 url = "https://api.random.org/json-rpc/4/invoke"
 
-class GeneralException(Exception):
+class GeneralError(Exception):
     def __init__(self, message):
         super().__init__(message)
 
-def requestnumber(apikey, min, max, numofints=1, allowduplicates = True) -> (int|List):
-    payload = {
-        "jsonrpc": "2.0",
-        "method": "generateIntergers",
-        "params": {
-            "apiKey": apikey,
-            "n": numofints,
-            "min": min,
-            "max": max,
-            "replacement": allowduplicates
-        },
-    }
-
-    response = requests.post(url, json=payload)
-
-    if response.status_code == 200:
-        raise GeneralException(f"{response.json()['error']['message']}")
+class RangeTooNarrowError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
     
-    return response.json()['result']['random']['data']
-
 
 class Generator():
-    def __init__(self, apikey):
-        self.apikey = apikey
+    def __init__(self, RANDOM_ORG_API_KEY):
+        self.apikey = RANDOM_ORG_API_KEY
 
-    def randint(self, min, max, numofints=1, allowduplicates = True) -> (int|List):
-        return requestnumber(self.apikey, min, max, numofints, allowduplicates)
+    def randint(self, minimum=1, maximum=100, numofints=1, allowduplicates = True) -> Union[int, List[int]]:
 
+        '''
+        generates a random integer between min and max.
+        default settings (no arguments) will generate one number between 1 to 100
+        '''
+
+        if numofints > (maximum-minimum) and not allowduplicates:
+            raise RangeTooNarrowError("Range of numbers should be more than or equal to numofints when allowduplicates is set to true.")
+        
+        if minimum >= maximum:
+            raise ValueError("min should be less than max")
+        
+        if numofints <= 0:
+            raise ValueError("numofints should be positive")
+
+        payload = {
+            "jsonrpc": "2.0",
+            "method": "generateIntegers",
+            "params": {
+                "apiKey": self.apikey,
+                "n": numofints,
+                "min": minimum,
+                "max": maximum,
+                "replacement": allowduplicates
+            },
+        }
+
+        print(payload)
+
+        response = requests.post(url, json=payload)
+
+        if response.status_code == 200:
+            print(response.json())
+            raise GeneralError(f"{response.json()['error']['message']} (error code {response.json()['error']['code']})")
+        
+        return response.json()['result']['random']['data']
+    
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+randgen = Generator(os.getenv("RANDOM_ORG_API_KEY"))
+
+print(randgen.randint(1, 3, 2))
